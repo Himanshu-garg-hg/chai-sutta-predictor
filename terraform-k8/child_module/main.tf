87885016d96b91f3077ae4456d0bd80f87fa9688
HP <himanshu951garg@gmail.com>
2026-03-18 16:19:27 +0530

    updated with workflow dispatch

--- terraform-k8/child_module/main.tf
+++ terraform-k8/child_module/main.tf
@@ -8,10 +8,16 @@ resource "azurerm_storage_account" "sa" {
     resource_group_name = azurerm_resource_group.rg.name    
     location = azurerm_resource_group.rg.location
     account_tier = "Standard"
-    account_replication_type = "LRS"
-
-  
+    account_replication_type = "LRS" 
 }
+
+resource "azurerm_storage_container" "container" {
+  name                  = "chai-sutta-container"
+  storage_account_id    = azurerm_storage_account.sa.id
+  container_access_type = "private"
+}
+
+
 resource "azurerm_container_registry" "acr" {
     name = var.acr_name
     resource_group_name = azurerm_resource_group.rg.name
